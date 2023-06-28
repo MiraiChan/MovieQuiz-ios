e999b6040c5fde6a4cd945939709c76e37ace517
Almira Khafizova <mimirka80@gmail.com>
2023-06-28 19:23:46 +0200

    Реализована загрузка фильмов в методе loadMovies, изменен IMDB API код

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -20,13 +20,31 @@ struct MoviesLoader: MoviesLoading {
     // MARK: - URL
       private var mostPopularMoviesUrl: URL {
           // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
-          guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_kiwxbi4y") else {
+          guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_bo4bl2fa") else {
               preconditionFailure("Unable to construct mostPopularMoviesUrl")
           }
           return url
       }
     
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
-    
+    //Используем networkClient для загрузки данных по URL
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case.success(let data):
+                do {
+                    //Преобразуем данные в MostPopularMovies, используя JSONDecoder
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    //Возвращаем результат в handler
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    //Если произошла ошибка при декодировании данных, передаем ошибку в handler
+                    handler(.failure(error))
+                }
+            case.failure(let error):
+                //Если произошла ошибка при загрузке данных, передаем ошибку в handler
+                handler(.failure(error))
+                }
+            }
+        }
     }
-}//- сам загрузчик, который будет реализовывать этот протокол
+//- сам загрузчик, который будет реализовывать этот протокол
